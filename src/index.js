4b5909a2dfc4f7864bc9282559b7375582d22491
Anvi Alex <epononanvialex@gmail.com>
2021-07-28 19:44:34 +0300

    Add CreateGame function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './style.css';
+import LeaderBoard from './app.js';
+
+const board = new LeaderBoard();
+
+// Create game
+const name = {name: "Mario Kart"};
+const createGame = name =>{
+  board.post('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', name)
+    .then(response => console.log(response))
+    .catch(err => console.log(err));
+}
+
+createGame(name);
