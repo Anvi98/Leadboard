7c10cf81578c55f2809d99625a830e4a96bb8fd7
Anvi Alex <epononanvialex@gmail.com>
2021-07-29 18:47:45 +0300

    Add background and header

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import './style.css';
 import { callApi, createGame, url } from './apiCalls.js';
+import princess from '../src/img/princess.jpg';
 
 const name = { name: 'Mario Kart' };
+const element = document.querySelectorAll('body');
 
+ // Add the image to our existing div.
+ const myIcon = new Image();
+ myIcon.src = princess;
+
+ element.appendChild(myIcon);
+
+background.style = ''
 createGame(name);
 callApi(url);
